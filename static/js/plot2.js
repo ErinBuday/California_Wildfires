47c881b77dbd6131510c5b04aff3236b13565127
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-07-28 19:29:37 -0400

    added guage plot

--- static/js/plot2.js
+++ static/js/plot2.js
@@ -196,6 +196,126 @@ function plot(year) {
 
 function init() {
     plot(0)
+    d3.json(fireLocations).then(function(data) {
+        currentTime = new Date()
+        currentTimeCount = 0
+        var currentMonth = currentTime.getMonth();
+        var currentHour = currentTime.getHours();
+
+        var currentMonth = 0;
+        var currentHour = 10;
+
+        var averageCount = data.data.length / 288;
+        var fireGuageNumber = 0;
+
+        for (var i=0; i < data.data.length; i++) {
+
+            var fireMonth = data.data[i][1][1] -1;
+            var fireHour = data.data[i][1][0];
+
+            if (currentMonth === fireMonth && currentHour === fireHour) {
+                currentTimeCount += 1;
+            };         
+        }
+
+        if (currentTimeCount < (averageCount * 0.05)) {
+            fireGuageNumber = 1;
+        } else if (currentTimeCount < (averageCount * 0.659)) {
+            fireGuageNumber = 2;
+        } else if (currentTimeCount < (averageCount * 1.341)) {
+            fireGuageNumber = 3;
+        } else if (currentTimeCount < (averageCount * 1.95)) {
+            fireGuageNumber = 4;
+        } else {
+            fireGuageNumber = 5;
+        }
+
+        var gauge_data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: fireGuageNumber,
+                title: { text: "Current Fire Risk" },
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    bar: {
+                        color: "white",
+                        thickness: 0.2
+                    },
+                    axis: { range: [[null], 5] },
+                    steps: [
+                        { range: [0, 1], color: 'rgb(44, 186, 0)'},
+                        { range: [1, 2], color: 'rgb(163, 255, 0)' },
+                        { range: [2, 3], color: 'rgb(255, 244, 0)'},
+                        { range: [3, 4], color: 'rgb(255, 167, 0)'},
+                        { range: [4, 5], color: 'rgb(255,0,0)'}
+
+                    ],
+                    // threshold: {
+                    //     line: { color: "white" },
+                    // }
+                }
+            }
+        ];
+
+        // Define Plot layout
+        var gauge_layout = { width: 500, height: 400, margin: { t: 0, b: 0 } };
+
+        if (fireGuageNumber === 1) {
+            var fireRisk = "very low risk";
+        } else if (fireGuageNumber === 2) {
+            var fireRisk = 'low risk';
+        } else if (fireGuageNumber === 3) {
+            var fireRisk = 'average risk';
+        } else if (fireGuageNumber === 4) {
+            var fireRisk = 'high risk';
+        } else if (fireGuageNumber === 5) {
+            var fireRisk = 'extremely high risk'
+        }
+
+        var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+        var timeName = currentTime.toLocaleTimeString('en-US', {hour: 'numeric',hour12: true});
+
+        if (currentMonth <= 3) {
+            monthDescription = "The winter months have the lowest frequency of fires, and most of them are human caused."
+        } else if (currentMonth < 6) {
+            monthDescription = "In spring frequency of fires increases steadily."
+        } else if (currentMonth < 8) {
+            if (currentMonth === 6) {
+                monthDescription = "The summer sports the highes frequency of forest fires, peaking in July. Watch out on the 4th! The majority of naturaly occuring fires happen now."
+            } else {
+                monthDescription = "The summer sports the highes frequency of forest fires. The majority of naturaly occuring fires happen now."
+            }
+        } else if (currentMonth < 10) {
+            monthDescription = "The frequency of fires steadily declines throughout the fall."
+        }
+
+        if (currentHour <= 6) {
+            hourDescription = "Early morning experiences the fewest number of fires. The cause of most fires around this time is never found out."
+        } else if (currentHour <= 11) {
+            hourDescription = "Fire incidents rise steadily throughout the morning."
+        } else if (currentHour <=5 ) {
+            if (currentHour === 14) {
+                if (currentMonth === 6) {
+                    hourDescription = "The afternoon experiences the greatest number of fires. 2pm in July? Be careful."
+                } else {
+                    hourDescription = "The afternoon experiences the greatest number of fires, peaking around now."
+                }
+            } else {
+                hourDescription = "The afternoon experiences the greatest number of fires, peaking around 2pm."
+            }
+        }else {
+            hourDescription = "Rates of fire incidents fall steadily throughout the evening."
+        }
+
+        var guageBlurb = `
+        <p>It is ${timeName} in the month of ${monthNames[currentMonth]} and the current fire assesment is ${fireRisk} ${monthDescription} ${hourDescription}`
+
+        // Display plot
+        Plotly.newPlot('gauge', gauge_data, gauge_layout);
+
+        d3.select('#gaugeBlurb').html(guageBlurb);
+    })
 };
 
 function optionChanged(value) {
